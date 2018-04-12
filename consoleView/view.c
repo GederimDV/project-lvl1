52fea158cab74347c750941dd2fc718a678d7137
Sukhorukov Maksim <sukhorukov.mv@reliab.tech>
2018-04-12 23:42:59 +0300

    New Makefile

--- consoleView/view.c
+++ consoleView/view.c
@@ -1,52 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-main()
-{
-	int N,i,j;
-	scanf("%d", &N);
-	char A[N][N];
-	for (i = 0; i < N; i++){
-	for (j = 0; j < N; j++){
-		/*A[i][j] = 10 + rand()% (20-10+1);*/
-		if (j == 1){
-			A[i][j] = *("|");
-		}
-		else if (j == 3) {
-			A[i][j] = *("|");
-		}
-                else {
-                        A[i][j] = *("-");
-                }
-		if (i == 1 && j != 1) {
-        	        A[i][j] = *("_");
-	        }
-       		else if (i == 3 && j != 3) {
-               		A[i][j] = *("_");
-        	}
-        	else {
-            	    A[i][j] = *("-");
-        	}
 
-		printf("%c", A[i][j]);
-		}
-	puts("\n");
-}
-printf("%d ", A[0][1]); //Проверка
-	return 0;
-}
-/*Хуево оформлен смотри мой код*/
-
-void viewField(char ** A, int fieldSize)
+void viewField(char ** field, int fieldSize)
 {
-    for(int i = 0; i < fieldSize; i++)
+    int i = 0;
+    int j = 0;
+    for(i = 0; i < fieldSize; i++)
     {
         printf("| ");
-        for(int j = 0; j < fieldSize; j++)
+        for(j = 0; j < fieldSize; j++)
         {
-            printf("%s |",field[i][j])      
+            printf("%d |",field[i][j]);
         }
         printf("\n");
     }
 }
 
-/*Должно быть примерно вот так*/
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "model/field.h"
+#include "consoleView/view.h"
 
 int main()
 {
+    int size = 3;
+    //char** field = createField(size);
 
-        return 0;
+    //viewField(field, size);
+
+    //deleteField(field, size);
+
+    printf("HEllo world");
+    return 0;
 }
