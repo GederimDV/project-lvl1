6e4cac20e736e97d8c1ab25eee334d38d2466450
Sukhorukov Maksim <sukhorukov.mv@reliab.tech>
2018-04-14 21:32:51 +0300

    Edit viewField

--- consoleView/view.c
+++ consoleView/view.c
@@ -2,14 +2,12 @@
 
 void viewField(char ** field, int fieldSize)
 {
-    int i = 0;
-    int j = 0;
-    for(i = 0; i < fieldSize; i++)
+    for(int i = 0; i < fieldSize; i++)
     {
         printf("| ");
-        for(j = 0; j < fieldSize; j++)
+        for(int j = 0; j < fieldSize; j++)
         {
-            printf("%d |",field[i][j]);
+            printf("%s | ", *field[i][j]);
         }
         printf("\n");
     }
