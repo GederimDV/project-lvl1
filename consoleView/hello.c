e1757b5a3bd8cac5901cc08d1349189ee2496112
GederimDV <dgederim@gmail.com>
2018-04-22 14:30:00 +0300

    new file and edit view.c

---
+++ consoleView/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+	char YN;
+	system("clear");
+	puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	printf("@ Hello, player! While you can play with a friend, continue?(yes/no) @\n");
+	puts("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	scanf("%s", &YN);
+	return YN
+}
+
